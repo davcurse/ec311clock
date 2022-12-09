803d8b460019c08c932ae87aa12c76bcff7e8262
David <87276972+davcurse@users.noreply.github.com>
2022-12-09 14:16:05 -0500

    Update stopwatch.v

--- ec311final/ec311final.srcs/sources_1/new/stopwatch.v
+++ ec311final/ec311final.srcs/sources_1/new/stopwatch.v
@@ -1,26 +1,33 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/09/2022 12:42:40 PM
-// Design Name: 
-// Module Name: stopwatch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module stopwatch(
-
+    input clk,
+    input resetn,
+    input toggle,
+    output reg [35:0] num,
+    output reg khz_clock
     );
+    
+    clock_divider cd(clk, resetn, khz_clock);
+    reg mode;
+    
+    always @ (posedge clk or negedge resetn) begin
+        if (!resetn) begin
+            num <= 0;
+            mode <= 0;
+        end else begin
+            if (toggle)
+                mode <= ~mode;
+        end
+    end //always
+    
+    always @ (posedge khz_clock or negedge resetn) begin
+        if (mode == 1) begin
+            if (num[3:0] == 9) begin
+                
+                
+            
+    
+    
+    
 endmodule
