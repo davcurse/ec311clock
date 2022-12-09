9810e99b03cfc076b272544283de5785219b7d37
slegaspi19 <99240284+slegaspi19@users.noreply.github.com>
2022-12-09 14:15:54 -0500

    Add files via upload

--- ec311final/ec311final.srcs/sources_1/new/timer.v
+++ ec311final/ec311final.srcs/sources_1/new/timer.v
@@ -21,6 +21,29 @@
 
 
 module timer(
-
+    input clk,
+    input kHz,
+    input reset,
+    input edit,
+    input up,
+    input down,
+    input left,
+    input right,
+    output reg [35:0] mytime
     );
+    reg [3:0] editing;
+    
+    always @ (posedge clk or negedge reset) begin
+        if (!reset) begin
+            mytime <= 0;
+            editing <= 0;
+        end else begin
+//            if (edit) begin 
+//                if (right) editing <= editing + 1;
+//                if (left) editing <= editing + 1;
+//                editing = editing % 9;
+//                if (up) 
+//        editing stuff
+        end
+    end
 endmodule
