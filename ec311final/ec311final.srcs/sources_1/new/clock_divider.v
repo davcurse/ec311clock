deed120bbaa204b71ed488c6773de8e457c1432b
David <87276972+davcurse@users.noreply.github.com>
2022-12-09 13:43:36 -0500

    commented out 1hz clock; only using 1khz clock

--- ec311final/ec311final.srcs/sources_1/new/clock_divider.v
+++ ec311final/ec311final.srcs/sources_1/new/clock_divider.v
@@ -2,39 +2,35 @@
 
 module clock_divider(
     input clock, reset,
-    output reg khz_out, hz_out
+    output reg khz_out
     );
     
     reg [31:0] khz_count = 32'b0, hz_count = 32'b0;
     
-    parameter khz_divider = 50000;
-    parameter hz_divider = 50000000;
+    parameter khz_divider = 100000;
+//    parameter hz_divider = 50000000;
     
     initial begin
         khz_out = 1'b0;
-        hz_out = 1'b0;
+//        hz_out = 1'b0;
     end
     always @ (posedge clock or negedge reset) begin
         if(!reset)begin
-            khz_count <= 32'b0;
-            hz_count <= 32'b0;
             khz_out <= 1'b0;
-            hz_out <= 1'b0;
+//            hz_out <= 1'b0;
          end else begin
-            if(khz_count < khz_divider-1) begin
+            khz_count <= khz_count + 1;
+            if(khz_count >= khz_divider-1)
                 khz_count <= 32'b0;
-                khz_out <=~khz_out;
-            end else begin
-                khz_count <= khz_count+1;
-                khz_out <= khz_out;
+            else
+                khz_out <= (khz_count<32'd100000/2)?1'b1:1'b0;
             end
-            if(hz_count == hz_divider-1) begin
-                hz_count <= 32'b0;
-                hz_out <=~hz_out;
-            end else begin
-                hz_count <= hz_count+1;
-                hz_out <= hz_out;
-            end            
-         end
-    end // always
+//            if(hz_count == hz_divider-1) begin
+//                hz_count <= 32'b0;
+//                hz_out <=~hz_out;
+//            end else begin
+//                hz_count <= hz_count+1;
+//                hz_out <= hz_out;
+//            end            
+    end //always
 endmodule
