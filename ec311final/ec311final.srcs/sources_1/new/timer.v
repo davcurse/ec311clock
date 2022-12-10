5b9cf756d010ea30f0ea607eec3f515667c25597
slegaspi19 <99240284+slegaspi19@users.noreply.github.com>
2022-12-09 21:15:41 -0500

    Add files via upload

--- ec311final/ec311final.srcs/sources_1/new/timer.v
+++ ec311final/ec311final.srcs/sources_1/new/timer.v
@@ -22,28 +22,52 @@
 
 module timer(
     input clk,
-    input kHz,
     input reset,
     input edit,
     input up,
     input down,
     input left,
     input right,
-    output reg [35:0] mytime
+    output reg [28:0] mytime,
+//    output reg [3:0] editing,
+    output reg [6:0] hours,
+    output reg [5:0] minutes, seconds,
+    output reg [9:0] milliseconds
     );
-    reg [3:0] editing;
-    
+//    reg [3:0] editing;
+//    reg [6:0] hours;
+//    reg [5:0] minutes, seconds;
+//    reg [9:0] milliseconds;
+    wire kiHz;
+    clock_divider kHz(clk, reset, kiHz);
     always @ (posedge clk or negedge reset) begin
         if (!reset) begin
             mytime <= 0;
-            editing <= 0;
+            hours <= 01;
+            minutes <= 01;
+            seconds <= 01;
+            milliseconds <= 01;
+//            editing <= 0;
         end else begin
-//            if (edit) begin 
-//                if (right) editing <= editing + 1;
-//                if (left) editing <= editing + 1;
-//                editing = editing % 9;
-//                if (up) 
-//        editing stuff
+            mytime <= {hours, minutes, seconds, milliseconds};
+            end
+    end
+//    always @ (posedge kiHz) begin
+    always @ (posedge clk) begin
+       if (!edit && {hours, minutes, seconds, milliseconds} != 0) begin
+            milliseconds <= milliseconds - 1;
+            if (milliseconds == 0) begin
+                milliseconds <= 999;
+                seconds <= seconds - 1;
+            if (seconds == 0) begin
+                seconds <= 59;
+                minutes <= minutes - 1;
+            if (minutes == 0) begin
+                minutes <= 59;
+                hours <= hours - 1;
+            end 
+            end
+            end
         end
     end
 endmodule
