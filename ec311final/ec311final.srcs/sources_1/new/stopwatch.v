2f1212624c5bc194ddb3fd191a58a5701d33d363
David <87276972+davcurse@users.noreply.github.com>
2022-12-09 20:45:07 -0500

    Update stopwatch.v

--- ec311final/ec311final.srcs/sources_1/new/stopwatch.v
+++ ec311final/ec311final.srcs/sources_1/new/stopwatch.v
@@ -4,30 +4,60 @@ module stopwatch(
     input clk,
     input resetn,
     input toggle,
-    output reg [35:0] num,
-    output reg khz_clock
+    output reg [28:0] num
+// for testing __
+//    output reg [6:0] hours,
+//    output reg [5:0] minutes,
+//    output reg [5:0] seconds,
+//    output reg [9:0] ms
     );
     
-    clock_divider cd(clk, resetn, khz_clock);
+    wire khz_clock;
     reg mode;
+    reg [6:0] hours;
+    reg [5:0] minutes;
+    reg [5:0] seconds;
+    reg [9:0] ms;
+    clock_divider cd(clk, resetn, khz_clock);
     
     always @ (posedge clk or negedge resetn) begin
         if (!resetn) begin
             num <= 0;
             mode <= 0;
+            hours <= 0;
+            minutes <= 0;
+            seconds <= 0;
+            ms <= 0;
         end else begin
+            num <= {hours,minutes,seconds,ms};
             if (toggle)
                 mode <= ~mode;
         end
     end //always
     
     always @ (posedge khz_clock or negedge resetn) begin
-        if (mode == 1) begin
-            if (num[3:0] == 9) begin
-                
-                
-            
-    
-    
-    
+        if (mode) begin
+            ms <= ms + 1;
+            if (ms == 999) begin
+                ms <= 0;
+                seconds <= seconds + 1;
+                if (seconds == 59) begin
+                    seconds <= 0;
+                    minutes <= minutes + 1;
+                    if (minutes == 59) begin
+                        minutes <= 0;
+                        hours <= hours + 1;
+                        if (hours == 99) begin
+                            mode <= 0;
+                            hours <= 0;
+                            minutes <= 0;
+                            seconds <= 0;
+                            ms <= 0;
+                        end
+                    end
+                end
+            end
+        end
+    end //always
+               
 endmodule
