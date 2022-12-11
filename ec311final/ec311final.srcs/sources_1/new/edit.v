6957f6719f90ada4ebe1afd3ee4396d5f78df44f
slegaspi19 <99240284+slegaspi19@users.noreply.github.com>
2022-12-11 14:54:06 -0500

    Add files via upload

---
+++ ec311final/ec311final.srcs/sources_1/new/edit.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/11/2022 08:08:35 AM
+// Design Name: 
+// Module Name: edit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module edit(
+    input clk,
+    input en,
+    input up,
+    input down,
+    input left,
+    input right,
+    input [28:0] time_in,
+    output reg [28:0] time_out,
+    output reg [1:0] editing
+    );
+    always @ (posedge clk) begin
+        if (!en) begin
+            editing <= 0;
+            time_out <= time_in;
+        end else begin
+            if (right) begin
+                editing <= ((editing + 3) + 1) % 3;
+            end
+            if (left) begin
+                editing <= ((editing + 3) - 1) % 3;
+            end
+            if (up) begin
+                case (editing)
+                    2'b00: begin
+                        time_out[28:22] <= time_out[28:22] + 1;
+                        if (time_out[28:22] == 2'd99) begin
+                            time_out[28:22] = 0;
+                        end
+                    end
+                    2'b01: begin
+                        time_out[21:16] <= time_out[21:16] + 1;
+                        if (time_out[21:16] == 2'd59) begin
+                            time_out[21:16] = 0;
+                        end
+                    end
+                    2'b10: begin
+                        time_out[15:10] <= time_out[15:10] + 1;
+                        if (time_out[15:10] == 2'd59) begin
+                            time_out[15:10] = 0;
+                        end
+                    end
+                endcase
+            end
+            if (down) begin
+                case (editing)
+                    2'b00: begin
+                        time_out[28:22] <= time_out[28:22] - 1;
+                        if (time_out[28:22] == 2'd00) begin
+                            time_out[28:22] = 2'd99;
+                        end
+                    end
+                    2'b01: begin
+                        time_out[21:16] <= time_out[21:16] - 1;
+                        if (time_out[21:16] == 2'd00) begin
+                            time_out[21:16] = 2'd59;
+                        end
+                    end
+                    2'b10: begin
+                        time_out[15:10] <= time_out[15:10] - 1;
+                        if (time_out[15:10] == 2'd00) begin
+                            time_out[15:10] = 2'd59;
+                        end
+                    end
+                endcase
+            end
+        end
+    end
+endmodule
--- ec311final/ec311final.srcs/sources_1/new/top.v
+++ ec311final/ec311final.srcs/sources_1/new/top.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12/09/2022 12:40:30 PM
+// Create Date: 12/11/2022 09:37:18 AM
 // Design Name: 
 // Module Name: top
 // Project Name: 
@@ -21,6 +21,47 @@
 
 
 module top(
-
+    input UP,
+    input DOWN,
+    input LEFT,
+    input RIGHT,
+    input TOGGLE,
+    input RST_BTN,
+    input CLK,
+    input EDIT,
+    input [1:0] SELECT,
+    output wire VGA_HS_O,       // horizontal sync output
+    output wire VGA_VS_O,       // vertical sync output
+    output wire [3:0] VGA_R,    // 4-bit VGA red output
+    output wire [3:0] VGA_G,    // 4-bit VGA green output
+    output wire [3:0] VGA_B     // 4-bit VGA blue output
     );
+    wire kHz_out;
+    wire UP_db;
+    wire DOWN_db;
+    wire LEFT_db;
+    wire RIGHT_db;
+    wire TOGGLE_db;
+    reg internal_RST;
+    reg timer_RST;
+    reg stopwatch_RST;
+    reg [28:0] out_twelve;
+    reg [28:0] out_twentyfour;
+    reg [28:0] out_timer;
+    reg [28:0] out_stopwatch;
+    clock_divider kHZ(CLK, RST_BTN, kHz_out);
+    debouncer UPdb(CLK, RST_BTN, UP, UP_db);
+    debouncer DOWNdb(CLK, RST_BTN, DOWN, DOWN_db);
+    debouncer LEFTdb(CLK, RST_BTN, LEFT, LEFT_db);
+    debouncer RIGHTdb(CLK, RST_BTN, RIGHT, RIGHT_db);
+    debouncer TOGGLEdb(CLK, RST_BTN, TOGGLE, TOGGLE_db);
+    always @ (*) begin
+        internal_RST = (((SELECT == 2'b00) | (SELECT == 2'b01)) ? RST_BTN : 1'b1);
+        timer_RST = (SELECT == 2'b10) ? RST_BTN : 1'b1;
+        stopwatch_RST = (SELECT == 2'b11) ? RST_BTN : 1'b1;
+    end
+    internal_clock internal(CLK, EDIT, UP_db, DOWN_db, LEFT_db, RIGHT_db, internal_RST, out_twelve, out_twentyfour);
+    timer timer(CLK, timer_RST, EDIT, UP_db, DOWN_db, LEFT_db, RIGHT_db);
+    stopwatch stopwatch(CLK, stopwatch_RST, TOGGLE_db, out_stopwatch);
+    VGA_display display(CLK, RST_BTN, SEGMENTS, VGA_HS_O, VGA_VS_O, VGA_R, VGA_G, VGA_B);
 endmodule
